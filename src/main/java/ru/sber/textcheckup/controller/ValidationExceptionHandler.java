50f4e2f54f9b963c24ffc38b741b214bca05d4ac
muryginds <muryginds@gmail.com>
2023-09-14 22:28:47 +0300

    validation exception handler added

---
+++ src/main/java/ru/sber/textcheckup/controller/ValidationExceptionHandler.java
@@ -0,0 +1,29 @@
+package ru.sber.textcheckup.controller;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.NonNull;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.stream.Collectors;
+
+@ControllerAdvice
+public class ValidationExceptionHandler extends ResponseEntityExceptionHandler {
+
+  @Override
+  protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                @NonNull HttpHeaders headers,
+                                                                @NonNull HttpStatusCode status,
+                                                                @NonNull WebRequest request) {
+    var errorText = ex.getBindingResult().getAllErrors().stream()
+        .map(DefaultMessageSourceResolvable::getDefaultMessage)
+        .collect(Collectors.joining(", "));
+
+    return ResponseEntity.badRequest().body(errorText);
+  }
+}
